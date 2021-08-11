67e4f7714308c449219e96cc45ce92c3f32243bf
Roman Meyerson <romanmayers@yahoo.com>
2021-08-11 15:26:48 +0300

    git ignore

--- build/CMakeFiles/AeroInvadersGame.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for AeroInvadersGame.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/AeroInvadersGame.dir/src/infra_classes/rcolor.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/AeroInvadersGame.dir/src/infra_classes/rcolor.cpp.o: \
- /home/romanmay/Development/CPP_Projects/CPP-Aero-Invaders/src/infra_classes/rcolor.cpp \
- /usr/include/stdc-predef.h \
- /home/romanmay/Development/CPP_Projects/CPP-Aero-Invaders/src/infra_classes/headers/rcolor.hpp
